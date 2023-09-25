748bf183b2a19a8dd331bff0cb3f1d0000a70cca
Mustafiz82e <mustafiz8260@gamil.com>
2023-09-25 20:39:46 +0600

    donation discription page completed

--- src/Pages/Donation-detail/DonationDetail.jsx
+++ src/Pages/Donation-detail/DonationDetail.jsx
@@ -1,13 +1,54 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const DonationDetail = () => {
-    
-    return (
-        <div>
-            <h1>donation Detail</h1>
-        </div>
-    );
+	const data = useParams();
+
+	const loadData = useLoaderData();
+	console.log(loadData);
+	// console.log(data.id);
+
+	// const [loadData , setLoadData] = useState()
+
+	// useEffect(() =>{
+	//     fetch('/DonationData.json')
+	//     .then(response => response.json())
+	//     .then(json => setLoadData(json))
+
+	// },[])
+
+	// console.log(loadData);
+
+	const foundData = loadData.find((item) => item.id == data.id);
+	console.log(foundData);
+
+    const {
+		title,
+		image,
+
+		cardColor,
+		buttonBackgroundColor,
+		textColor,
+		categoryBackgroundColor,
+		category,
+		id,price,description
+	} = foundData;
+
+   
+
+	return (
+		<div>
+			<div className="relative">
+                <img src={image} alt="" className="h-[600px] w-full object-cover" />
+                <button className={`btn text-white absolute bottom-0 m-8`} style={{background: textColor}}>Donate {price}</button>
+
+            </div>
+            <div>
+                <h1 className="text-5xl my-5 font-semibold">{title}</h1>
+                <p>{description}</p>
+            </div>
+		</div>
+	);
 };
 
 export default DonationDetail;
