acb401e79576a4992b3b402fd4e21d1106008eab
Mustafiz82e <mustafiz8260@gamil.com>
2023-09-27 00:25:12 +0600

    Error Page updated

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>404 NOt found</h1>
+        <div className='w-screen h-screen flex justify-center items-center text-center'>
+            <div className='space-y-5'>
+            <h1 className='text-3xl lg:text-5xl'>Oops</h1>
+            <h1 className='text-2xl lg:text-3xl'>404 Page not Found</h1>
+            <div className='flex justify-center'>
+                <Link to="/"><button className='btn btn-primary'>Go Back</button></Link>
+            </div>
+
+            </div>
         </div>
     );
 };
